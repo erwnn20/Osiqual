bb444d6d61edb4f737994e9bd34faf5706a61b93
verwann <erwann.varlet@ynov.com>
2025-07-24 01:23:21 +0200

    update configs files for local deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,35 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+import fs from 'fs';
+
+function getAllJsFiles(dir) {
+    let results = [];
+    // const list = ;
+    fs.readdirSync(dir)
+        .forEach(file => {
+            const fullPath = path.join(dir, file);
+            const stat = fs.statSync(fullPath);
+            if (stat && stat.isDirectory()) {
+                results = results.concat(getAllJsFiles(fullPath));
+            } else if (fullPath.endsWith('.js')) {
+                results.push(fullPath);
+            }
+        });
+    return results;
+}
+
+const scriptFiles = getAllJsFiles('resources/js/scripts');
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                ...scriptFiles,
+            ],
             refresh: true,
         }),
         tailwindcss(),
